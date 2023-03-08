a5bd088e5fc270d0cea9cbb2142a354f2709f3b8
Benkalio <officialtennysonbk@gmail.com>
2023-03-08 15:27:30 +0100

    updated admin and registered new post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
+
+# ADD ROWS TO ADMIN POST
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('title', 'date_created', 'author')
+
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
